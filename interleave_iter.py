c793f7ad45154eb263ca944fd749f9d3beb010ef
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2023-03-19 16:20:43 +0100

    Not so useful docstring.

--- interleave_iter.py
+++ interleave_iter.py
@@ -1,25 +1,18 @@
 import itertools
 
-from typing import Generator
 from typing import TypeVar
 from typing import Iterable
 from typing import Iterator
+from typing import NoReturn
+
 
 T = TypeVar("T")
 
 
 def interleave(*iters: Iterable[T]) -> Iterator[T]:
+    """
+    Interleave iterables.
+    """
     for z in itertools.chain.from_iterable(itertools.zip_longest(*iters)):
         if z is not None:
             yield z
-
-def main() -> int:
-    l1 = ["b", None, "m", None, "yzh"]
-    l2 = ["a", "c",]
-    for x in interleave(l2, l1):
-        print(x)
-    return 0
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
