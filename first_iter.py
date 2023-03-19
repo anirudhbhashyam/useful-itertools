9b6e19807b3223ab93681d05d1fd0ea3cf77ab88
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2023-03-19 16:14:46 +0100

    Added tests.

--- first_iter.py
+++ first_iter.py
@@ -1,19 +1,27 @@
 from itertools import chain, repeat
 
 from typing import Iterable
+from typing import Iterator
 from typing import TypeVar
+from typing import NoReturn
+
+import pytest
+
 
 T = TypeVar("T")
 
 
-def first(g: Iterable[T], *, n: int = 1) -> zip[tuple[bool, T]]:
+def first(g: Iterable[T], *, n: int = 1) -> Iterator[tuple[bool, T]]:
     return zip(chain(repeat(True, n), repeat(False)), g)
 
 
-def main() -> int:
-    print(list(first(range(1, 10))))
-    return 0
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
+@pytest.mark.parametrize(
+    ("iterable"),
+    [
+        range(1, 3), 
+        [(True, 1), (False, 2)],
+        "ghe"
+    ]
+)
+def test_first(iterable: Iterable[T]) -> NoReturn:
+    assert next(first(iterable))[1] == next(iter(iterable))
