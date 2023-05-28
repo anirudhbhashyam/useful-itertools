2eac6c42c2330b8c56cc64084a78edcd5050ceb6
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2023-05-28 08:51:10 +0530

    Add tests.

--- tuples_iter.py
+++ tuples_iter.py
@@ -1,12 +1,15 @@
 import itertools
 
-from typing import Any
-from typing import Generator
+import pytest
+
 from typing import Iterable
 from typing import Iterator
+from typing import TypeVar
+
+T = TypeVar("T")
 
 
-def take_tuples(g: Iterable[Any], *, n: int = 3) -> Iterator[tuple[Any, ...]]:
+def take_tuples(g: Iterable[T], *, n: int = 3) -> Iterator[tuple[T, ...]]:
     """
     Progress an iterable in steps of `n`.
     """
@@ -14,8 +17,22 @@ def take_tuples(g: Iterable[Any], *, n: int = 3) -> Iterator[tuple[Any, ...]]:
     return zip(*(itertools.islice(g, i, None, n) for i, g in enumerate(itertools.tee(it, n))))
 
 
+@pytest.mark.parametrize(
+    "input, n, expected_output", 
+    [
+        ([1, 2, 3, 4, 5, 6, 7], 3, [(1, 2, 3), (4, 5, 6)]),
+        ([1, 2, 3, 4, 5, 6, 7], 2, [(1, 2), (3, 4), (5, 6)]),
+        ("abfg", 2, [("a", "b"), ("f", "g")]),
+        (("-1", "-2", "-3", "-10"), 2, [("-1", "-2"), ("-3", "-10")]),
+        ((), 2, []),
+    ]
+)
+def test_take_tuples(input: Iterable[T], n: int, expected_output: list[tuple[T, ...]]):
+    assert list(take_tuples(input, n = n)) == expected_output
+
+
 def main() -> int:
-    l = [1, 2, 3, 4, 5, 6, 7]
+    l = (1, 2, 3)
     for i in take_tuples(l, n = 3):
         print(i)
     return 0
